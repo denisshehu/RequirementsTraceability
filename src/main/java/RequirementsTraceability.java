92d58047e8741553b34405700ef7127321df9914
Denis Shehu <denisshehu.n@gmail.com>
2021-02-25 23:26:44 +0100

    matchType is read using the Scanner class

--- src/main/java/RequirementsTraceability.java
+++ src/main/java/RequirementsTraceability.java
@@ -3,18 +3,27 @@ import models.Dataset;
 import models.Requirement;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class RequirementsTraceability {
     private static int matchType = 0;
 
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Match type not defined, default 0");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose trace-link generation technique (0, 1, 2, or 3):");
+        String input = scanner.nextLine();
+
+        if (input.length() == 0) {
+            System.out.println("Trace-link generation technique not defined, set to 0 by default.");
         } else {
             try {
-                matchType = Integer.parseInt(args[0]);
+                matchType = Integer.parseInt(input);
+                if (matchType > 3) {
+                    System.out.println("Not a valid trace-link generation technique, set to 0 by default.");
+                }
             } catch (NumberFormatException e) {
-                System.out.println("Invalid match type, default 0");
+                System.out.println("Not a valid trace-link generation technique, set to 0 by default.");
             }
         }
 
